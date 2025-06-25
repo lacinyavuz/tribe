28773009e4feffc4fe28ce4a6b8f1d863d8bbc39
ahmet.yavuz <ahmet.yavuz@sterling-dcs.com>
2025-06-26 00:24:13 +0100

    refactors

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ function insertRandomEvent() {
 }
 
 if (process.env.NODE_ENV !== 'production') {
-  // setInterval(insertRandomEvent, 1000);
+  setInterval(insertRandomEvent, 1000);
 }
 
 app.use(express.static(path.join(__dirname, '../client', 'dist')));
